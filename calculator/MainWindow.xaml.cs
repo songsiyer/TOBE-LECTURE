39b4e7f310ecfc044d54785fa56bb61fdd7e978c
songsiyer <songsiyer@hotmail.com>
2021-09-02 17:56:39 +0900

    c#계산기

---
+++ calculator/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using calculator;
+using Newtonsoft.Json;
+
+namespace calculator
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void numPadClick(object sender, RoutedEventArgs e)
+        {
+            string clicked = ((Button)sender).Tag.ToString();
+            if(num_textbox.Text == "0")
+            {
+                num_textbox.Text = clicked;
+            }
+            else
+            {
+                num_textbox.Text += clicked;
+            }
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            num_textbox.Text = "0";
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            string tmp = num_textbox.Text;
+            num_textbox.Text = tmp.Substring(0,tmp.Length-1);
+        }
+    }
+}
